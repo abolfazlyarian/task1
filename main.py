0d9a30114777c259fc2626fc92a78f407f403522
abolfazlyarian <yarianabolfazl@gmail.com>
2023-07-14 12:28:03 +0330

    database manger

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pymongo
 import csv
 import os
 import time
+from transformers import AutoTokenizer
+
 
 class mongoDBManager:
     """
@@ -38,6 +40,18 @@ class mongoDBManager:
             collection_name (str): The name of the collection to create.
         """
         self.database.create_collection(collection_name)
+
+    def insert_dataframe_data(self, collection_name, dataframe):
+        """
+        Inserts data from a DataFrame into the specified collection.
+
+        Args:
+            collection_name (str): The name of the collection to insert data into.
+            dataframe (pandas.DataFrame): The DataFrame containing the data.
+        """
+        collection = self.database[collection_name]
+        records = dataframe.to_dict(orient="records")
+        collection.insert_many(records)
 
     def insert_csv_data(self, collection_name, csv_file):
         """
@@ -75,6 +89,9 @@ class dataExtractor:
         super(dataExtractor, self).__init__()
         self.actions_df = None
         self.book_df = None
+        self.merge_df = None
+        self.feature_encodings = None
+        self.data_df = None 
 
     def read_csv_data(self, csv_url, columns=[]):
         self.df = pd.read_csv(csv_url)
@@ -83,59 +100,14 @@ class dataExtractor:
 
         return self.df
     
-    def pre_process_actions(self, df):
-        self.df = df.dropna()
-
-        # adfs 
-        self.df['creation_date'] = pd.to_datetime(self.df['creation_date']).dt.date
-        self.df = self.df.groupby('account_id', group_keys=True).apply(lambda x: x.sort_values('creation_date', ascending=False)).reset_index(drop=True)
-        self.df = self.df[pd.to_datetime(self.df['creation_date']).dt.year == 2022]
-
-        # dsaf
-        self.df['score'] = 5
-        previous_id = None
-        previous_day = None
-        previous_idx = None
-        for index, row in self.df.iterrows():
-            current_id = row['account_id']
-            if previous_id != current_id:
-                previous_idx = None
-            current_day = pd.to_datetime(row['creation_date']).day_of_year
-            if current_id == previous_id and current_day <= previous_day:
-                if previous_idx == None:
-                    self.df.at[index, 'score'] = self.df.at[index, 'score'] - (previous_day-current_day)//29
-                else:
-                    self.df.at[index, 'score'] = self.df.at[previous_idx, 'score'] - (previous_day-current_day)//29
-                previous_idx = index
-            previous_id = current_id
-            previous_day = current_day
-
-        # self.df.to_csv('dataset/action-pre.csv', index=False)
-        return self.df
-
-
-    def pre_process_books(self, df):
-        self.df = df
-        rating_mean = self.df['rating'].mean()
-        self.df['rating'] = self.df['rating'].fillna(value=rating_mean)
-
-        return self.df
-    
-    def merge_tables(self, left_df, right_df, on='book_id'):
-        pass
-
-
-class actionsExtractor(dataExtractor):
-    def __init__(self, csv_url):
-        super(actionsExtractor, self).__init__()
+    def pre_process_actions(self, csv_url):
         self.actions_df = self.read_csv_data(csv_url, columns=['account_id', 'book_id', 'creation_date']).dropna()
-
-    def pre_process(self):
-
         self.actions_df['creation_date'] = pd.to_datetime(self.actions_df['creation_date']).dt.date
-        self.actions_df = self.actions_df.groupby('account_id', group_keys=True).apply(lambda x: x.sort_values('creation_date', ascending=False)).reset_index(drop=True)
+        # self.actions_df = self.actions_df.groupby('account_id', group_keys=True).apply(lambda x: x.sort_values('creation_date', ascending=False)).reset_index(drop=True)
         self.actions_df = self.actions_df[pd.to_datetime(self.actions_df['creation_date']).dt.year == 2022]
-        self.rating()
+        self.actions_df['score'] = 5
+        # self.rating()
+        self.actions_df['creation_date'] = pd.to_datetime(self.actions_df['creation_date'])
 
     def rating(self):
         self.actions_df['score'] = 5
@@ -155,53 +127,32 @@ class actionsExtractor(dataExtractor):
                 previous_idx = index
             previous_id = current_id
             previous_day = current_day
-    
 
-class bookExtractor(dataExtractor):
-    def __init__(self, csv_url):
-        super(bookExtractor, self).__init__()
+    def pre_process_book(self, csv_url):
         self.book_df = self.read_csv_data(csv_url)
-
-    def pre_process(self):
         rating_mean = self.book_df['rating'].mean()
         self.book_df['rating'] = self.book_df['rating'].fillna(value=rating_mean)
 
+    def merge_tables(self, on='book_id'):
+        self.merge_df = self.actions_df.merge(self.book_df, on=on, how='inner')
+        self.merge_df['rating_count'] = self.merge_df.groupby('account_id')['book_id'].transform('count')
+        self.tokenizer(column='categories')
+        self.data_df = pd.concat([self.merge_df[['account_id', 'book_id','creation_date', 'score', 'price', 'number_of_page',
+                                                'PhysicalPrice', 'rating', 'rating_count', ]],
+                                  pd.DataFrame(self.feature_encodings['input_ids'])], axis=1)
+        
+        self.data_df.columns = self.data_df.columns.astype(str)
 
+    def tokenizer(self, column='categories'):
+        tokenizer = AutoTokenizer.from_pretrained("HooshvareLab/bert-base-parsbert-uncased")
+        self.feature_encodings = tokenizer(self.merge_df[column].tolist(), truncation=True, padding=True, max_length=64)
 
-
-def dataExtractor1():
-    action_df = pd.read_csv("dataset/actions.csv").dropna()
-    action_df.columns = ['account_id', 'book_id', 'creation_date']
-    action_df['creation_date'] = pd.to_datetime(action_df['creation_date']).dt.date
-    
-    action_df = action_df.groupby('account_id', group_keys=True).apply(lambda x: x.sort_values('creation_date', ascending=False)).reset_index(drop=True)
-    action_df = action_df[pd.to_datetime(action_df['creation_date']).dt.year == 2022]
-
-    action_df['score'] = 5
-    previous_id = None
-    previous_day = None
-    previous_idx = None
-    for index, row in action_df.iterrows():
-        current_id = row['account_id']
-        if previous_id != current_id:
-            previous_idx = None
-        current_day = pd.to_datetime(row['creation_date']).day_of_year
-        if current_id == previous_id and current_day <= previous_day:
-            if previous_idx == None:
-                action_df.at[index, 'score'] = action_df.at[index, 'score'] - (previous_day-current_day)//29
-            else:
-                action_df.at[index, 'score'] = action_df.at[previous_idx, 'score'] - (previous_day-current_day)//29
-            previous_idx = index
-        previous_id = current_id
-        previous_day = current_day
-
-    action_df.to_csv('dataset/action-pre.csv', index=False)
 
 
 def main():
     # MongoDB connection details
     mongodb_url = "mongodb://localhost:27017/"
-    database_name = "mydatabase"
+    database_name = "taaghche"
 
     # Create an instance of MongoDBManager
     mongo_manager = mongoDBManager(mongodb_url, database_name)
@@ -209,23 +160,22 @@ def main():
     # Create two collections
     mongo_manager.create_collection("actions")
     mongo_manager.create_collection("book_data")
+    mongo_manager.create_collection("merge")
 
-
-
+    data_extractor = dataExtractor()
+    data_extractor.pre_process_actions(csv_url='dataset/actions.csv')
+    data_extractor.pre_process_book(csv_url='dataset/book_data.csv')
+    data_extractor.merge_tables()
 
 
     # Insert data from the first CSV file into table1
-    mongo_manager.insert_csv_data("actions", "path/to/first.csv")
-
-    # Insert data from the second CSV file into table2
-    mongo_manager.insert_csv_data("table2", "path/to/second.csv")
+    mongo_manager.insert_dataframe_data("actions", data_extractor.actions_df)
+    mongo_manager.insert_dataframe_data("book_data", data_extractor.book_df)
+    mongo_manager.insert_dataframe_data("merge", data_extractor.data_df)
 
     # Watch the directory for changes
-    mongo_manager.watch_directory("path/to/watched_directory")
+    # mongo_manager.watch_directory("path/to/watched_directory")
 
 
 if __name__ == "__main__":
-    dataext = dataExtractor()
-    x = dataext.read_csv_data(csv_url='dataset/actions.csv', columns=['account_id', 'book_id', 'creation_date'])
-    y = dataext.pre_process_actions(x)
-    print(y)
+    main()
